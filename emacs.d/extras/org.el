d509297e2674f1ab96f4eed19ae7a82669716f34
simon. <109069254+flameesy@users.noreply.github.com>
2026-02-18 18:22:16 +0100

    Refactor Org-mode setup and remove unused comments
    Removed comments and organized Org-mode setup for clarity.

--- emacs.d/extras/org.el
+++ emacs.d/extras/org.el
@@ -1,42 +1,10 @@
 ;;; org.el --- Org mode setup  -*- lexical-binding: t; -*-
 ;;;
-;;; Usage: Append or require this file from init.el for some software
-;;; development-focused packages.
-;;;
-;;; Org-mode is a fantastically powerful package. It does a lot of things, which
-;;; makes it a little difficult to understand at first.
-;;;
-;;; We will configure Org-mode in phases. Work with each phase as you are
-;;; comfortable.
-;;;
-;;; YOU NEED TO CONFIGURE SOME VARIABLES! The most important variable is the
-;;; `org-directory', which tells org-mode where to look to find your agenda
-;;; files.
-
-;;; See "org-intro.txt" for a high-level overview.
-
-;;; Contents:
-;;;
-;;;  - Critical variables
-;;;  - Phase 1: editing and exporting files
-;;;  - Phase 2: todos, agenda generation, and task tracking
-;;;  - Phase 3: extensions (org-roam, etc.)
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;;   Critical variables
+;;;   Variables
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;; These variables need to be set for Org-mode's full power to be unlocked!
-;;;
-;;; You can read the documentation for any variable with `C-h v'. If you have
-;;; Consult configured (see the `base.el' file) then it should help you find
-;;; what you're looking for.
-
-;;; Phase 1 variables
-
-;;; Phase 2 variables
 
 ;; Agenda variables
 (setq org-directory "~/Documents/org/") ; Non-absolute paths for agenda and
@@ -67,55 +35,30 @@
 ;; Org-refile: where should org-refile look?
 (setq org-refile-targets 'FIXME)
 
-;;; Phase 3 variables
-
 ;; Org-roam variables
 (setq org-roam-directory "~/Documents/org-roam/")
 (setq org-roam-index-file "~/Documents/org-roam/index.org")
 
-;;; Optional variables
-
-;; Advanced: Custom link types
-;; This example is for linking a person's 7-character ID to their page on the
-;; free genealogy website Family Search.
-(setq org-link-abbrev-alist
-      '(("family_search" . "https://www.familysearch.org/tree/person/details/%s")))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;;   Phase 1: editing and exporting files
+;;;   Editing and exporting files, todos, agenda generation, and task tracking
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package org
   :hook ((org-mode . visual-line-mode)  ; wrap lines at word breaks
-         (org-mode . flyspell-mode))    ; spell checking!
-
+         (org-mode . flyspell-mode))    ; spell checking!         
   :bind (:map global-map
               ("C-c l s" . org-store-link)          ; Mnemonic: link → store
-              ("C-c l i" . org-insert-link-global)) ; Mnemonic: link → insert
+              ("C-c l i" . org-insert-link-global)) ; Mnemonic: link → insert             
   :config
   (require 'oc-csl)                     ; citation support
-  (add-to-list 'org-export-backends 'md)
-
+  (add-to-list 'org-export-backends 'md)    
   ;; Make org-open-at-point follow file links in the same window
   (setf (cdr (assoc 'file org-link-frame-setup)) 'find-file)
-
   ;; Make exporting quotes better
-  (setq org-export-with-smart-quotes t)
-  )
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;;   Phase 2: todos, agenda generation, and task tracking
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Yes, you can have multiple use-package declarations. It's best if their
-;; configs don't overlap. Once you've reached Phase 2, I'd recommend merging the
-;; config from Phase 1. I've broken it up here for the sake of clarity.
-(use-package org
-  :config
+  (setq org-export-with-smart-quotes t)     
+             
   ;; Instead of just two states (TODO, DONE) we set up a few different states
   ;; that a task can be in. Run
   ;;     M-x describe-variable RET org-todo-keywords RET
@@ -149,11 +92,3 @@
             (todo)))
           ("w" "Work" agenda ""
            ((org-agenda-files '("work.org")))))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;;   Phase 3: extensions
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; TODO
