72a6591928befdf69dfdcae2114531c01f8ba11f
simon. <109069254+flameesy@users.noreply.github.com>
2026-02-18 19:44:34 +0100

    Add files via upload

--- emacs.d/extras/org.el
+++ emacs.d/extras/org.el
@@ -1,55 +1,13 @@
 ;;; org.el --- Org mode setup  -*- lexical-binding: t; -*-
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; Variables
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Core locations
 (setq org-directory (expand-file-name "~/Documents/org/"))
 (setq org-roam-directory (expand-file-name "~/Documents/org-roam/"))
 (setq org-roam-index-file (expand-file-name "index.org" org-roam-directory))
 
-;; Helper: monthly work log file path
 (defun knoglerdev/org-work-monthly-file ()
   "Return absolute path of the current monthly work log file."
   (expand-file-name (format-time-string "work-%Y-%m.org") org-directory))
-
-;; Include inbox/work hub + all monthly work logs in agenda/search
-(setq org-agenda-files
-      (append
-       (mapcar (lambda (f) (expand-file-name f org-directory))
-               '("inbox.org" "work.org"))
-       (directory-files org-directory t "^work-[0-9]\\{4\\}-[0-9]\\{2\\}\\.org$")))
-
-;; Default tags
-(setq org-tag-alist
-      '((:startgroup)
-        ("home" . ?h)
-        ("work" . ?w)
-        ("school" . ?s)
-        (:endgroup)
-        (:newline)
-        (:startgroup)
-        ("one-shot" . ?o)
-        ("project"  . ?j)
-        ("tiny"     . ?t)
-        (:endgroup)
-        ("meta")
-        ("review")
-        ("reading")))
-
-;; Refile targets: current file + headings up to level 3 in agenda files
-(setq org-refile-targets
-      '((nil :maxlevel . 3)
-        (org-agenda-files :maxlevel . 3)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; Org mode setup
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun knoglerdev/org--maybe-ticket-line (s)
   "Return an indented ticket line for timelog entries, or empty string."
@@ -57,6 +15,27 @@
     (if (string-empty-p tix)
         ""
       (concat "  Ticket: " tix "\n"))))
+
+(setq org-agenda-files
+      (append
+       (mapcar (lambda (f) (expand-file-name f org-directory))
+               '("inbox.org" "work.org"))
+       (when (file-directory-p org-directory)  ; <-- guard
+         (directory-files org-directory t "^work-[0-9]\\{4\\}-[0-9]\\{2\\}\\.org$"))))
+
+(setq org-tag-alist
+      '((:startgroup)
+        ("home" . ?h) ("work" . ?w) ("school" . ?s)
+        (:endgroup)
+        (:newline)
+        (:startgroup)
+        ("one-shot" . ?o) ("project" . ?j) ("tiny" . ?t)
+        (:endgroup)
+        ("meta") ("review") ("reading")))
+
+(setq org-refile-targets
+      '((nil :maxlevel . 3)
+        (org-agenda-files :maxlevel . 3)))
 
 (use-package org
   :hook ((org-mode . visual-line-mode)
@@ -66,34 +45,22 @@
               ("C-c a" . org-agenda)
               ("C-c l" . org-store-link))
   :config
-  ;; Citation support (built-in in newer org; harmless if available)
   (require 'oc-csl nil t)
-
-  ;; Export tweaks
   (add-to-list 'org-export-backends 'md)
   (setq org-export-with-smart-quotes t)
-
-  ;; Open file links in same window
   (setf (cdr (assoc 'file org-link-frame-setup)) #'find-file)
 
-  ;; TODO workflow
   (setq org-todo-keywords
         '((sequence "TODO(t)" "WAITING(w@/!)" "STARTED(s!)" "|"
                     "DONE(d!)" "OBSOLETE(o@)")))
 
-  ;; Keep state-change logs tidy
   (setq org-log-into-drawer t)
-
-  ;; Refile UX
   (setq org-outline-path-complete-in-steps nil)
   (setq org-refile-use-outline-path 'file)
   (setq org-refile-allow-creating-parent-nodes 'confirm)
 
-  ;; Capture templates
   (setq org-capture-templates
-        `(
-          ;; Inbox
-          ("c" "Default Capture" entry
+        `(("c" "Default Capture" entry
            (file ,(expand-file-name "inbox.org" org-directory))
            "* TODO %?\n%U\n%i")
 
@@ -101,13 +68,11 @@
            (file ,(expand-file-name "inbox.org" org-directory))
            "* TODO %?\n%U\n%i\n%a")
 
-          ;; Monthly timelog (your Notepad++-style, but organized)
           ("tl" "Time log (monthly, multiline)" item
-           (file+olp+datetree ,(lambda () (knoglerdev/org-work-monthly-file)))
-           "- %^{From}-%^{To}: %^{Context}\n  %?\n%(knoglerdev/org--maybe-ticket-line \"%^{Ticket (optional, Jira/GDI/blank)}\")"
+           (file+olp+datetree ,(knoglerdev/org-work-monthly-file)) ; pre-evaluated
+           "- %^{From}-%^{To}: %^{Context}\n  %?\n  Ticket: %^{Ticket (optional)}\n"
            :empty-lines 1)
 
-          ;; Structured work captures (keep using work.org as hub)
           ("w" "Work")
           ("wm" "Work meeting" entry
            (file+headline ,(expand-file-name "work.org" org-directory) "Meetings")
@@ -117,24 +82,16 @@
            (file+headline ,(expand-file-name "work.org" org-directory) "Reports")
            "** TODO %?\n%U\n%i\n%a")))
 
-  ;; Custom agenda commands
   (setq org-agenda-custom-commands
         '(("n" "Agenda and All Todos"
-           ((agenda)
-            (todo)))
+           ((agenda) (todo)))
           ("w" "Work"
-           ((agenda "" ((org-agenda-files (list (expand-file-name "work.org" org-directory))))))))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; Org-roam
-;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+           ((agenda "" ((org-agenda-files
+                         (list (expand-file-name "work.org" org-directory))))))))))
 
 (use-package org-roam
   :after org
-  :custom
-  (org-roam-directory org-roam-directory)
-  (org-roam-index-file org-roam-index-file)
   :config
-  (org-roam-db-autosync-mode 1))
+  (setq org-roam-directory (expand-file-name "~/Documents/org-roam/"))
+  ;(org-roam-db-autosync-mode 1)
+  )
